009e8db1c1e2ed5e59cc7b8c7c7eefa524438b5c
Darío <dario.mart.lop.03@gmail.com>
2025-08-08 01:37:08 +0200

    finished :3

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,23 @@
 /*   By: dario <dario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 22:44:02 by dario             #+#    #+#             */
-/*   Updated: 2025/08/07 23:02:27 by dario            ###   ########.fr       */
+/*   Updated: 2025/08/08 01:36:14 by dario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	Zombie *heapZ = newZombie("HeapZombie");
-    
-    heapZ->announce();
-    delete (heapZ);
-    
-    randomChump("StackZombie");
+	for (int i = 1; i < argc; i++)
+	{
+		Zombie	*heapZombie = newZombie(argv[i]);
+		heapZombie->announce();
+		delete heapZombie;
+	}
+
+	for (int i = 1; i < argc; i++)
+		randomChump(argv[i]);
+	
 	return (0);	
 }
